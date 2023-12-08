80061d90f3099ace96a31cc41c02144cd85d99c7
vixuowis <vixuowis@users.noreply.github.com>
2023-12-08 17:42:51 +0000

    refactor: mrkl demo

--- streamlit_agent/mrkl_demo.py
+++ streamlit_agent/mrkl_demo.py
@@ -1,13 +1,17 @@
+import os
+
 from pathlib import Path
 
 import streamlit as st
 
 from langchain import SQLDatabase
 from langchain.agents import AgentType
-from langchain.agents import initialize_agent, Tool
+from langchain.agents import initialize_agent, Tool, load_tools
 from langchain.callbacks import StreamlitCallbackHandler
 from langchain.chains import LLMMathChain
-from langchain.llms import OpenAI
+
+# from langchain.llms import OpenAI
+from langchain.chat_models import AzureChatOpenAI
 from langchain.utilities import DuckDuckGoSearchAPIWrapper
 from langchain_experimental.sql import SQLDatabaseChain
 
@@ -24,78 +28,47 @@ SAVED_SESSIONS = {
 }
 
 st.set_page_config(
-    page_title="MRKL", page_icon="🦜", layout="wide", initial_sidebar_state="collapsed"
+    page_title="有梗 AI", page_icon="🦜", layout="wide", initial_sidebar_state="collapsed"
 )
 
-"# 🦜🔗 MRKL"
-
-# Setup credentials in Streamlit
-user_openai_api_key = st.sidebar.text_input(
-    "OpenAI API Key", type="password", help="Set this to run your own custom questions."
-)
-
-if user_openai_api_key:
-    openai_api_key = user_openai_api_key
-    enable_custom = True
-else:
-    openai_api_key = "not_supplied"
-    enable_custom = False
+"# 🦜🔗 有梗 AI"
 
 # Tools setup
-llm = OpenAI(temperature=0, openai_api_key=openai_api_key, streaming=True)
-search = DuckDuckGoSearchAPIWrapper()
-llm_math_chain = LLMMathChain.from_llm(llm)
-db = SQLDatabase.from_uri(f"sqlite:///{DB_PATH}")
-db_chain = SQLDatabaseChain.from_llm(llm, db)
+llm = AzureChatOpenAI(
+    openai_api_version="2023-05-15",
+    openai_api_key="2864ce19a46540b2a0943df607ca6225",
+    openai_api_base="https://autoagents-ca-east.openai.azure.com/",
+    deployment_name="gpt-4",
+    streaming=True,
+)
+
+# Initialize agent
+llm_math_chain = LLMMathChain(llm=llm)
 tools = [
-    Tool(
-        name="Search",
-        func=search.run,
-        description="useful for when you need to answer questions about current events. You should ask targeted questions",
-    ),
     Tool(
         name="Calculator",
         func=llm_math_chain.run,
         description="useful for when you need to answer questions about math",
-    ),
-    Tool(
-        name="FooBar DB",
-        func=db_chain.run,
-        description="useful for when you need to answer questions about FooBar. Input should be in the form of a question containing full context",
-    ),
+        return_direct=True,
+    )
 ]
 
-# Initialize agent
-mrkl = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
 
 with st.form(key="form"):
-    if not enable_custom:
-        "Ask one of the sample questions, or enter your API Key in the sidebar to ask your own custom questions."
-    prefilled = st.selectbox("Sample questions", sorted(SAVED_SESSIONS.keys())) or ""
-    user_input = ""
-
-    if enable_custom:
-        user_input = st.text_input("Or, ask your own question")
-    if not user_input:
-        user_input = prefilled
-    submit_clicked = st.form_submit_button("Submit Question")
+    user_input = st.text_input("您想要点啥啊？")
+    submit_clicked = st.form_submit_button("开始找点乐子！")
 
 output_container = st.empty()
 if with_clear_container(submit_clicked):
     output_container = output_container.container()
     output_container.chat_message("user").write(user_input)
 
-    answer_container = output_container.chat_message("assistant", avatar="🦜")
+    answer_container = output_container.chat_message("有梗 AI", avatar="🦜")
     st_callback = StreamlitCallbackHandler(answer_container)
 
     # If we've saved this question, play it back instead of actually running LangChain
     # (so that we don't exhaust our API calls unnecessarily)
-    if user_input in SAVED_SESSIONS:
-        session_name = SAVED_SESSIONS[user_input]
-        session_path = Path(__file__).parent / "runs" / session_name
-        print(f"Playing saved session: {session_path}")
-        answer = playback_callbacks([st_callback], str(session_path), max_pause_time=2)
-    else:
-        answer = mrkl.run(user_input, callbacks=[st_callback])
+    answer = agent.run(user_input, callbacks=[st_callback])
 
     answer_container.write(answer)
